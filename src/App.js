5a9c87f21ae5b092f6fcb71ca05d991c451f3273
ibaraness <idan.baraness@gmail.com>
2018-05-12 21:41:16 +0300

    Added bmr calculator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BmrCalculator from './cantainers/bmr-calculator/BmrCalculator';
 
+import {
+  Form, Input, Radio, Grid, Segment
+} from 'semantic-ui-react'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+    this.handleForm = this.handleForm.bind(this);
+  }
+
+  handleForm(e, { name, value }) {
+    console.log("e", this.state);
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid columns={1} padded>
+          <Grid.Column>
+            <BmrCalculator bmrState={this.state} onBmrChange={this.handleForm}></BmrCalculator>
+          </Grid.Column>
+        </Grid>
+
       </div>
     );
   }
